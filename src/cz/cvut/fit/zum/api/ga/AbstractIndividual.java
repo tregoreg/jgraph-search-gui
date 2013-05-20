2490d1951c05ea107e21992bd7f0d433c43e411b
Tomas Barton <barton.tomas@gmail.com>
2013-05-20 13:21:10 +0200

    fixed compare behaviour to match standard definition

--- src/cz/cvut/fit/zum/api/ga/AbstractIndividual.java
+++ src/cz/cvut/fit/zum/api/ga/AbstractIndividual.java
@@ -8,40 +8,31 @@ import java.util.List;
  */
 public abstract class AbstractIndividual implements Comparable<AbstractIndividual> {
 
-    
     public abstract double getFitness();
-    
+
     public abstract void countFitness();
-        
+
     public abstract boolean getGen(int j);
-    
+
     public abstract AbstractIndividual deepCopy();
-    
+
     public abstract List<AbstractIndividual> cross(AbstractIndividual i);
-    
+
     public abstract void mutate();
-    
-   
-    /* ################################################################### */
-    /*    BASICALLY, YOU DO NOT NEED TO TOUCH CODE BELLOW THIS LINE ;-)    */
-    /* ################################################################### */
+
     /**
-     * Compares this object with the specified object by value of fitness
-     * funcion for order. When you want to minimalize fitness, you have to
-     * reverse positive and negative cases. This definition works for
-     * 1maximizing value of fitness function.
+     * Must be compatible with Java compare usage:
+     * +1 if this is greater than the other 
+     * -1 if this is smaller than the other, 0 if they are equal
+     * 
      *
-     * Returns -1 in case that this object is greater than o. Returns 0 in case
-     * that this.equals(o) == true. Returns +1 in case that this object is
-     * smaller than o.
      */
     @Override
     public int compareTo(AbstractIndividual another) {
         if (this.getFitness() > another.getFitness()) {
-            return -1;
-        }
-        if (this.getFitness() < another.getFitness()) {
             return 1;
+        } else if (this.getFitness() < another.getFitness()) {
+            return -1;
         }
         return 0;
     }
